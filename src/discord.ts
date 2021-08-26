c16775f474bab96f87ba6772b151554ed62992db
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-08-26 20:47:22 +1000

    Add basic music command

--- src/discord.ts
+++ src/discord.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { ipcMain } from "electron";
 import Eris from "eris";
 
 dotenv.config();
@@ -6,6 +7,8 @@ dotenv.config();
 var bot = Eris(process.env.TOKEN);
 
 var message = "hello";
+
+const playCommand = "!play";
 
 bot.on("ready", () => {
   console.log("Ready!");
@@ -19,16 +22,55 @@ bot.on("messageCreate", (msg) => {
   if (msg.content === "!hello") {
     bot.createMessage(msg.channel.id, message);
   }
+  if (msg.content.startsWith(playCommand)) {
+    // If the message content starts with "!play "
+    if (msg.content.length <= playCommand.length + 1) {
+      // Check if a filename was specified
+      bot.createMessage(msg.channel.id, "Please specify a filename.");
+      return;
+    }
+    if (!(msg.channel as any).guild) {
+      // Check if the message was sent in a guild
+      bot.createMessage(
+        msg.channel.id,
+        "This command can only be run in a server."
+      );
+      return;
+    }
+    if (!msg.member.voiceState.channelID) {
+      // Check if the user is in a voice channel
+      bot.createMessage(msg.channel.id, "You are not in a voice channel.");
+      return;
+    }
+    const filename = msg.content.substring(playCommand.length + 1); // Get the filename
+    bot
+      .joinVoiceChannel(msg.member.voiceState.channelID)
+      .catch((err) => {
+        // Join the user's voice channel
+        bot.createMessage(
+          msg.channel.id,
+          "Error joining voice channel: " + err.message
+        ); // Notify the user if there is an error
+        console.log(err); // Log the error
+      })
+      .then((connection) => {
+        if (connection) {
+          if (connection.playing) {
+            // Stop playing if the connection is playing something
+            connection.stopPlaying();
+          }
+          connection.play(filename); // Play the file and notify the user
+          bot.createMessage(msg.channel.id, `Now playing **${filename}**`);
+          connection.once("end", () => {
+            bot.createMessage(msg.channel.id, `Finished **${filename}**`); // Say when the file has finished playing
+          });
+        }
+      });
+  }
 });
 
 bot.connect();
 
-window.onbeforeunload = () => {
-  bot.disconnect({ reconnect: false });
-};
-
-export const api = {
-  updateMessage: (newMessage: string) => {
-    message = newMessage;
-  },
-};
+ipcMain.on("update-message", (_, arg) => {
+  message = arg;
+});
--- src/preload.ts
+++ src/preload.ts
@@ -1,4 +1,10 @@
-import { api } from "./discord";
 import { contextBridge } from "electron";
+import { ipcRenderer } from "electron";
+
+const api = {
+  updateMessage: (newMessage: string) => {
+    ipcRenderer.send("update-message", newMessage);
+  },
+};
 
 contextBridge.exposeInMainWorld("discord", api);

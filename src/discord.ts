e4d6673c727740ceabf31281217131af4f95f917
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-08-28 22:55:38 +1000

    Move to discord.js and fix issues with production build

--- src/discord.ts
+++ src/discord.ts
@@ -1,98 +1,52 @@
-import { ipcMain } from "electron";
-import Eris from "eris";
-import ytdl from "ytdl-core";
+import { ipcMain, app } from "electron";
+import Discord from "discord.js";
+import ytdl from "ytdl-core-discord";
 
-var bot: Eris.Client | null = null;
-var currentVoiceConnection: Eris.VoiceConnection | null = null;
+const client = new Discord.Client();
+const broadcasts: Record<string, Discord.VoiceBroadcast> = {};
 
-ipcMain.on("connect", (event, token) => {
-  if (bot) {
-    bot.disconnect({ reconnect: false });
+client.on("message", async (msg) => {
+  if (msg.content === "!join") {
+    if (!msg.guild) {
+      // Check if the message was sent in a guild
+      msg.reply("This command can only be run in a server.");
+      return;
+    }
+    if (!msg.member.voice.channel) {
+      // Check if the user is in a voice channel
+      msg.reply("You are not in a voice channel.");
+      return;
+    }
+    await msg.member.voice.channel.join();
   }
+});
+
+ipcMain.on("connect", async (event, token) => {
   if (!token) {
     event.reply("disconnect");
     event.reply("error", "Error connecting to bot: invalid token");
     return;
   }
-  bot = Eris(token);
-
-  bot.on("ready", () => {
+  try {
+    await client.login(token);
     event.reply("ready");
     event.reply("message", "Connected");
-  });
-
-  bot.on("error", (err) => {
+  } catch (err) {
     event.reply("error", "Error connecting to bot " + err.message);
-    console.error(err);
-    bot.disconnect({ reconnect: false });
-  });
-
-  bot.on("disconnect", () => {
-    event.reply("disconnect");
-    event.reply("stopAll");
-    event.reply("message", "Disconnected");
-    bot = null;
-    currentVoiceConnection = null;
-  });
-
-  bot.on("messageCreate", (msg) => {
-    if (msg.content.startsWith("!join")) {
-      if (!(msg.channel as any).guild) {
-        // Check if the message was sent in a guild
-        bot.createMessage(
-          msg.channel.id,
-          "This command can only be run in a server."
-        );
-        return;
-      }
-      if (!msg.member.voiceState.channelID) {
-        // Check if the user is in a voice channel
-        bot.createMessage(msg.channel.id, "You are not in a voice channel.");
-        return;
-      }
-      bot
-        .joinVoiceChannel(msg.member.voiceState.channelID)
-        .catch((err) => {
-          bot.createMessage(
-            msg.channel.id,
-            "Error joining voice channel: " + err.message
-          ); // Notify the user if there is an error
-          console.log(err); // Log the error      console.log(err); // Log the error
-        })
-        .then((connection) => {
-          currentVoiceConnection = connection || null;
-        });
-    }
-  });
-
-  bot.on("voiceChannelLeave", () => {
-    currentVoiceConnection = null;
-  });
-
-  bot.connect();
+  }
 });
 
 ipcMain.on("play", async (event, url, id) => {
-  if (!bot) {
-    event.reply(
-      "error",
-      "Not connected please update token in connection menu"
-    );
-    event.reply("stop", id);
-    return;
+  // TODO: Check client ready
+
+  let broadcast: Discord.VoiceBroadcast;
+  if (id in broadcasts) {
+    broadcast = broadcasts[id];
+  } else {
+    broadcast = client.voice.createBroadcast();
+    broadcasts[id] = broadcast;
   }
-  if (!currentVoiceConnection) {
-    event.reply(
-      "error",
-      "Not in a voice channel use !join to add bot to a voice channel"
-    );
-    event.reply("stop", id);
-    return;
-  }
-  if (currentVoiceConnection.playing) {
-    // Stop playing if the connection is playing something
-    currentVoiceConnection.stopPlaying();
-  }
+
   const valid = ytdl.validateURL(url);
   if (!valid) {
     event.reply("error", "Invalid url");
@@ -100,23 +54,26 @@ ipcMain.on("play", async (event, url, id) => {
     return;
   }
   const info = await ytdl.getInfo(url);
-  const stream = ytdl.downloadFromInfo(info, { quality: "highestaudio" });
-  currentVoiceConnection.play(stream);
+  const stream = await ytdl(url);
+  const dispatcher = broadcast.play(stream, { type: "opus" });
+
+  client.voice.connections.forEach((connection) => {
+    connection.play(broadcast);
+  });
+
   event.reply("play", id);
   event.reply("message", `Now playing ${info.videoDetails.title}`);
-  currentVoiceConnection.once("end", () => {
+
+  dispatcher.on("finish", () => {
     event.reply("stop", id);
     event.reply("message", `Finished ${info.videoDetails.title}`);
   });
 });
 
 ipcMain.on("stop", (event, id) => {
-  if (!currentVoiceConnection) {
-    event.reply("error", "No voice connection to stop");
-    return;
-  }
-  if (currentVoiceConnection.playing) {
-    currentVoiceConnection.stopPlaying();
+  event.reply("stop", id);
+  if (broadcasts[id]) {
+    broadcasts[id].dispatcher.pause();
   }
 });
 
@@ -133,4 +90,13 @@ ipcMain.on("validateUrl", async (event, url, id) => {
   event.reply("validation", valid, id);
 });
 
-export default bot;
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+  client?.destroy();
+});
+
+app.on("quit", () => {
+  client?.destroy();
+});
--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,14 +1,25 @@
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/index.ts',
+  entry: ["./src/index.ts"],
+  // Don't package discord.js and it's dependencies as it doesn't work with webpack
+  externals: {
+    "discord.js": "commonjs2 discord.js",
+    "ffmpeg-static": "commonjs2 ffmpeg-static",
+    opusscript: "commonjs2 opusscript",
+    "ytdl-core-discord": "commonjs2 ytdl-core-discord",
+  },
   // Put your normal webpack config below here
   module: {
-    rules: require('./webpack.rules'),
+    rules,
   },
+  plugins: plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
 };

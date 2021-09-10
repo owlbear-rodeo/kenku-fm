782c31a3c1db5e18c1b4602ef9d79d1abae046bd
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-09-10 10:57:08 +1000

    Add back app icon

--- forge.config.js
+++ forge.config.js
@@ -1,53 +1,54 @@
 module.exports = {
+  packagerConfig: {
+    icon: "./src/assets/icon",
+  },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
-        name: 'kenku_forge',
+        name: "kenku_forge",
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: [
-        'darwin',
-      ],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     [
-      '@electron-forge/plugin-webpack',
+      "@electron-forge/plugin-webpack",
       {
-        mainConfig: './webpack.main.config.js',
+        mainConfig: "./webpack.main.config.js",
         renderer: {
-          config: './webpack.renderer.config.js',
+          config: "./webpack.renderer.config.js",
           entryPoints: [
             {
-              html: './src/index.html',
-              js: './src/renderer.ts',
-              name: 'main_window',
-              "preload": {
-                "js": "./src/preload.ts"
-              }
+              html: "./src/index.html",
+              js: "./src/renderer.ts",
+              name: "main_window",
+              preload: {
+                js: "./src/preload.ts",
+              },
             },
           ],
         },
-        devContentSecurityPolicy: ''
+        devContentSecurityPolicy: "",
       },
     ],
     [
       "@timfish/forge-externals-plugin",
       {
-        "externals": ["ffmpeg-static", "opusscript", "prism-media", "discord.js"],
-        "includeDeps": true
-      }
-    ]
+        externals: ["ffmpeg-static", "opusscript", "prism-media", "discord.js"],
+        includeDeps: true,
+      },
+    ],
   ],
 };

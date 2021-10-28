330a3eccbdbbcdbfc860173bf825d285310db59e
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-10-28 18:43:28 +1100

    Remove remote time animation

--- src/remote/Player.tsx
+++ src/remote/Player.tsx
@@ -72,7 +72,6 @@ const TimeSlider = styled(Slider)({
   "& .MuiSlider-thumb": {
     width: 8,
     height: 8,
-    transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
     "&:before": {
       boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
     },

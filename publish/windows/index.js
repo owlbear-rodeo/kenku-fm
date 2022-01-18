f5b7b76a6c354dc58c6fae22804d0a1a9504fe48
Nicola Thouliss <53867736+nthouliss@users.noreply.github.com>
2022-01-18 15:25:15 +1100

    Add script to package windows app

---
+++ publish/windows/index.js
@@ -0,0 +1,22 @@
+const electronInstaller = require("electron-winstaller");
+const path = require("path");
+
+async function createApp(dir, version) {
+    try {
+        await electronInstaller.createWindowsInstaller({
+            appDirectory: path.join(dir, "out", "Kenku FM-win32-x64"),
+            outputDirectory: path.join(dir, "out", "windows"),
+            loadingGif: path.join("src", "assets", "loading.gif"),
+            setupIcon: path.join(dir, "src", "assets", "setup.ico"),
+            iconUrl: path.join(dir, "src", "assets", "setup.ico"),
+            noMsi: true,
+            exe: "kenku-fm.exe",
+            setupExe: `kenku-fm-${version}-setup.exe`
+          });
+    } catch (e) {
+        console.log(`Error occured: ${e.message}`)
+    }    
+}
+
+const args = process.argv.slice(2)
+createApp(args[0], args[1])

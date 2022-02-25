38e40910381c158cbfad1ab328610f95fad4253f
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-02-25 16:06:10 +1100

    Move update code to a new file
    Co-Authored-By: Nicola Thouliss <53867736+nthouliss@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app, autoUpdater, dialog, BrowserWindow, components, session, shell } from "electron";
+import { app, BrowserWindow, components, session, shell } from "electron";
 import "./menu";
 import icon from "./assets/icon.png";
 import { getUserAgent } from "./main/userAgent";
@@ -11,6 +11,9 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
   app.quit();
+} else if (app.isPackaged) {
+  // Setup auto update
+  require("./update");
 }
 
 const createWindow = (): void => {
@@ -64,34 +67,6 @@ app.whenReady().then(async () => {
   createWindow();
   spoofUserAgent();
 });
-
-const server = "https://hazel-owlbear-rodeo.vercel.app/"
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
-
-autoUpdater.setFeedURL({ url })
-
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 1200000)
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
-
-autoUpdater.on('error', message => {
-  console.error('There was a problem updating the application')
-  console.error(message)
-})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits

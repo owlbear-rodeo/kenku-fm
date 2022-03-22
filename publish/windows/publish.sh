13a7e21c7a24d49f633de38e669ddd177b8e677a
Nicola Thouliss <53867736+nthouliss@users.noreply.github.com>
2022-03-22 17:56:23 +1100

    Edit windows publish steps

--- publish/windows/publish.sh
+++ publish/windows/publish.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-WINDOWS_CERT_PASSWORD=$1
 CERTIFICATE_PFX=certificate.pfx;
 echo "${WINDOWS_CERT_P12}" | base64 -d > $CERTIFICATE_PFX;
-
-cd ./publish/windows/ && yarn install --non-interactive --frozen-lockfile && cd ../..
-
-node ./publish/windows/index.js "./" v1.0.0-beta-2

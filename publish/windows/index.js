6567ce535d634eae9be12bc31cd10406a55b7c11
Nicola Thouliss <53867736+nthouliss@users.noreply.github.com>
2022-03-23 11:05:11 +1100

    Remove path for certificate input on script

--- publish/windows/index.js
+++ publish/windows/index.js
@@ -6,13 +6,13 @@ async function createApp(dir, version, certPassword) {
         await electronInstaller.createWindowsInstaller({
             appDirectory: path.join(dir, "out", "Kenku FM-win32-x64"),
             outputDirectory: path.join(dir, "out", "windows"),
-            loadingGif: path.join("src", "assets", "loading.gif"),
+            loadingGif: path.join(dir, "src", "assets", "loading.gif"),
             setupIcon: path.join(dir, "src", "assets", "setup.ico"),
             iconUrl: path.join(dir, "src", "assets", "setup.ico"),
             noMsi: true,
             exe: "kenku-fm.exe",
             setupExe: `kenku-fm-${version}-setup.exe`,
-            signWithParams: `/a /f "${path.join(dir, "certificate.pfx")}" /p "${certPassword}" /tr "http://timestamp.comodoca.com" /td "sha256" /fd "sha256"`
+            signWithParams: `/a /f "certificate.pfx" /p "${certPassword}" /tr "http://timestamp.comodoca.com" /td "sha256" /fd "sha256"`
           });
     } catch (e) {
         console.log(`Error occured: ${e.message}`)

5e9881430f192b9c1e59653164af90b387c08277
Nicola Thouliss <53867736+nthouliss@users.noreply.github.com>
2023-02-24 13:20:35 +1100

    Fix forge error on make

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -2,15 +2,12 @@ const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 const isProduction = process.env.CI;
 
-module.exports = {
+const config = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
   entry: "./src/index.ts",
-  output: {
-    sourceMapFilename: "[id].[contenthash].min.js"
-  },
   // Put your normal webpack config below here
   module: {
     rules: require("./webpack.rules"),
@@ -19,18 +16,7 @@ module.exports = {
     extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
   devtool: "source-map",
-  mode: isProduction ? 'production' : 'development',
-  plugins: [
-    isProduction && new SentryWebpackPlugin({
-          org: "owlbear-rodeo",
-          project: "kenku-fm",
-          include: ["./.webpack/main/**/*"],
-          urlPrefix: "~/.webpack/main",
-          authToken: process.env.SENTRY_AUTH_TOKEN,
-          release: `Kenku-FM@${process.env.VERSION}`,
-          sourceMapReference: false
-        }),
-  ].filter(n => n),
+  mode: "development",
   externals: {
     opusscript: "commonjs2 opusscript",
     "prism-media": "commonjs2 prism-media",
@@ -38,3 +24,19 @@ module.exports = {
     "discord.js": "commonjs2 discord.js",
   },
 };
+
+if (isProduction) {
+  config.mode = "production"
+  config.plugins = [
+    new SentryWebpackPlugin({
+      org: "owlbear-rodeo",
+      project: "kenku-fm",
+      include: ["./.webpack/main/**/*"],
+      urlPrefix: "~/.webpack/main",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      release: `Kenku-FM@${process.env.VERSION}`,
+    }),
+  ]
+}
+
+module.exports = config;

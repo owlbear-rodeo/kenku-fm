b01a17348d6b5f6559b8f33a750a04e44d2d8097
Mitchell McCaffrey <mitchemmc@gmail.com>
2023-03-21 12:08:20 +1100

    Ensure discord client exists when destroying it

--- src/main/managers/DiscordManager.ts
+++ src/main/managers/DiscordManager.ts
@@ -21,7 +21,9 @@ export class DiscordManager {
     ipcMain.off("DISCORD_DISCONNECT", this._handleDisconnect);
     ipcMain.off("DISCORD_JOIN_CHANNEL", this._handleJoinChannel);
     ipcMain.off("DISCORD_LEAVE_CHANNEL", this._handleLeaveChannel);
-    severus.discordDestroy(this.client);
+    if (this.client) {
+      severus.discordDestroy(this.client);
+    }
     this.client = undefined;
     this.audio = undefined;
   }

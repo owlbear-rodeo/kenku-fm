a6cb2e28be73696c5cf5ea7f16be599711599911
nthouliss <53867736+nthouliss@users.noreply.github.com>
2025-08-29 17:05:50 +1000

    Update mac package script

--- publish/mac/package.js
+++ publish/mac/package.js
@@ -2,17 +2,18 @@ import { exit } from "node:process";
 import path from "node:path";
 import { createDMG } from "electron-installer-dmg";
 
-function make(appDir, makeDir, appName, background, icon) {
-
-  const outPath = path.resolve(makeDir, `${appName}.dmg`);
+function make() {
+  const __dirname = import.meta.dirname;
+  const srcDir = path.resolve(__dirname, "..", "..");
+  const appName = "Kenku FM"
 
   const dmgConfig = {
     overwrite: true,
     name: appName,
-    background,
-    icon,
-    appPath: path.resolve(appDir, `${appName}.app`),
-    out: path.dirname(outPath),
+    background: path.join(srcDir, "src", "assets", "dmg-background.png"),
+    icon: path.join(srcDir, "src", "assets", "setup.icns"),
+    appPath: path.join(srcDir, "out", `Kenku FM-darwin-${process.arch}`, `${appName}.app`),
+    out: path.join(srcDir, "out", `Kenku FM-darwin-${process.arch}`)
   };
 
   createDMG(dmgConfig)
@@ -25,36 +26,4 @@ function make(appDir, makeDir, appName, background, icon) {
     });
 }
 
-const args = process.argv.slice(2);
-const appDir = path.resolve(args[0]);
-const makeDir = args[1];
-const appName = args[2];
-const background = args[3];
-const icon = args[4];
-
-if (appDir === undefined) {
-  console.log("appDir is undefined");
-  exit(1);
-}
-
-if (makeDir === undefined) {
-  console.log("Make directory is undefined");
-  exit(1);
-}
-
-if (appName === undefined) {
-  console.log("App name is undefined");
-  exit(1);
-}
-
-if (background === undefined) {
-  console.log("Background is undefined");
-  exit(1);
-}
-
-if (icon === undefined) {
-  console.log("Icon is undefined");
-  exit(1);
-}
-
-make(appDir, makeDir, appName, background, icon);
+make();

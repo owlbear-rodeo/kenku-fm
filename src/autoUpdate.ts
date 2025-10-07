18d0d0a93303b634f8ddb5535722dc2074504fc4
nthouliss <53867736+nthouliss@users.noreply.github.com>
2025-10-07 17:30:43 +1100

    Update autoUpdate.ts

--- src/autoUpdate.ts
+++ src/autoUpdate.ts
@@ -16,11 +16,7 @@ function checkForAppUpdates() {
 export function runAutoUpdate(window: BrowserWindow) {
   if (process.platform === "win32" || process.platform == "darwin") {
     const server = "https://download.kenku.fm";
-    let url = `${server}/update/${process.platform}/${app.getVersion()}`;
-
-    if (process.platform === "darwin" && process.arch === "arm64") {
-      url = `${server}/update/${process.platform}_arm64/${app.getVersion()}`;
-    }
+    let url = `${server}/update/${process.platform}/${process.arch}/${app.getVersion()}`;
 
     autoUpdater.setFeedURL({ url });
 
